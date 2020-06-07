6e9e0467f9e272f58af03b734343ac45f9f737c7
amirhasance <amirhasan.sadatmand.1392@gmail.com>
2020-06-08 02:31:03 +0430

    just save Here and goodnight

--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+#TODO  : Try pip install django_cleanup , https://stackoverflow.com/questions/16041232/django-delete-filefield
+class Question(models.Model):
+    
+    question = models.CharField(max_length=500  , blank= False , null = False)
+    image = models.ImageField(upload_to = '/questions' , null =True , blank = True)
+    caution_help = models.CharField(max_length=500  , blank= False , null = False)
+    
+    class Type_Of_Question(models.Model):
+        Four_choices = '4G' , _('4Gozine')
+        True_False = 'TF'  , _('True_False')
+        Tashrihi = 'TA'  , _('Tashrihi')  
+        
+    type_of_question = models.CharField(
+                                        max_length = 2,
+                                        choices=Type_Of_Question.choices,
+                                        default=Type_Of_Question.Tashrihi
+                                        )
+    choiceOne = models.CharField(max_length=500 , null=True , blank = True)
+    choiceTwo = models.CharField(max_length=500 , null= True , blank = True)
+    choiceThree = models.CharField(max_length=500 , null = True , blank = True)
+    choiceFour = models.CharField(max_length=500 , null = True , blank = True)
+    
+    
+ 
+
+    
--- klass/models.py
+++ klass/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Teacher(User):
+    image = models.ImageField(upload_to='/Teachers')
+    
+    
+    pass
+
+
+class Student(User):
+    image = models.ImageField(upload_to='/Students')
+    pass
+
+
+class Dars(models.Model):
+    name  = models.CharField(max_length = 200)
+    image = models.ImageField(upload_to='/Classes')
+    Time_created = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(Student)
+    
+    pass
