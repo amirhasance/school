0ac78647e3bc197877bfeeba05b2081e0421e6e9
amirhasance <amirhasan.sadatmand.1392@gmail.com>
2020-06-08 15:43:44 +0430

    settings are set

--- school/settings.py
+++ school/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'school.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR , "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+# media or media_root
+MEDIA_ROOT = os.path.join(BASE_DIR,'media_root')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staic_root')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
